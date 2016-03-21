6d5c97fb25366a1723f8908130b9fc4595c52853
joao pedro abreu de souza <joaopedroabreudesouza@cryptolab.net>
2016-03-20 21:37:43 -0300

    tente dar pull agora

--- interface/interface.c
+++ interface/interface.c
@@ -1,5 +1,4 @@
 #include <grafo/grafo.h>
-#include <string/bstrlib.h>
 #include <util/dbg.h>
 #include <stdio.h>
 
@@ -12,7 +11,6 @@ int main(int argc, char *argv[]){
 	Grafo *g;
 	char acao;
 	int rc;
-error:
 	if(argc == 1){
 		char buffer[MAXBUF];
 		printf("Digite o nome do arquivo contendo as informações do grafo: \n");
@@ -27,25 +25,95 @@ error:
 		arquivo = fopen(buffer, "r");
 		check(arquivo, "O arquivo não pode ser aberto. Modo de uso: ./interface <nome do arquivo>");
 	}
+
 	g = recuperarGrafo(arquivo);
+	check(g, "Falha na criação do grafo. Seu arquivo é no formato correto?");
 	while(acao != 's'){
+		printf("Digite a ação:");
 		rc = scanf("%c", &acao);
 		check(rc == 1, "Passe uma das ações aceitas: 'l':listar vértices e custos\n 'a':adicionar vértice\n 'A':adicionar aresta \n \
 				'r':remover vértice \n 'R':remover aresta \n 'v':verificar se o grafo é conexo \n 'c':encontrar caminho\n 's':sair");
 
-		/*
-		 *	TODO: identificação das opções do usuário
-		 *
-		 */
+		unsigned int inicio, fim, v;
+		double valor;
+		switch(acao){
+			case 'l':
+				imprimeGrafo(g);
+				break;
+			case 'a':
+				addVertice(g);
+				printf("Vértice adicionado à rede.\n");
+				break;
+			case 'A':
+				printf("\tDigite os indices inicial e final da aresta:\n");
+				rc = scanf("%u %u", &inicio, &fim);
+				check(rc == 2, "É necessário que sejam digitados dois índices.");
+				addAresta(g, inicio, fim);
+				break;
+			case 'r':
+				printf("\tDigite o indice do vértice:\n");
+				rc = scanf("%u", &v);
+				check(rc == 1, "É necessário que seja digitado um índice.");
+				liberaVertice((void *) &v);
+				break;
+			case 'R':
+				/*
+				 *
+				 * TODO
+				 *	- Manutenção de arestas
+				 * 	
+				 * */
+				break;
+			case 'v':
+				if(estaConexo(g)) printf("\tO Grafo é conexo.\n");
+				else printf("\tO Grafo não é conexo.\n");
+				break;
+			case 'c':
+				printf("\tDigite os indices inicial e final para saber se há um caminho:\n");
+				rc = scanf("%u %u", &inicio, &fim);
+				check(rc == 2, "É necessário que sejam digitados dois índices.");
+				if(existeCaminho(g, inicio, fim)){
+					printf("\tExiste um caminho, e o caminho mínimo é:\n");
+					Lista_imprimir(djikstra(g, inicio, fim));
+				}
+				break;
+			case 's':
+				imprimeGrafo(g);
+				if(estaConexo(g)) printf("\tO Grafo é conexo.\n");
+				else printf("\tO Grafo não é conexo.\n");
+				printf("\t----\n");
+				break;
+			default:
+				printf("Ações aceitas: 'l':listar vértices e custos\n 'a':adicionar vértice\n 'A':adicionar aresta \n \
+				'r':remover vértice \n 'R':remover aresta \n 'v':verificar se o grafo é conexo \n 'c':encontrar caminho\n 's':sair\n");
+				break;
+		}
 	}
 
 	fclose(arquivo);
 	return 0;
+error:
+	return 1;
 }
 
 Grafo *recuperarGrafo(FILE *arquivo){
-	/*
-	 *	TODO: Parsing do arquivo para um grafo
-	 */
+	int tamanho, rc, i = 0, vizinho, id;
+	//double nome;
+	rc = fread(&tamanho, sizeof(int), 1, arquivo);
+	check(rc == 1, "Falha na leitura do tamanho da lista.");
+	Grafo *grafo = initGrafo(tamanho);
+
+	for(i = 0; i < tamanho; i++){
+		grafo = addVertice(grafo);
+		id = grafo->numeroVertices - 1;
+		//rc = fread(&id, sizeof(int), 1, arquivo);
+		//rc = fread(&nome, sizeof(double), 1, arquivo);
+		do{
+			rc = fread(&vizinho, sizeof(int), 1, arquivo);
+			grafo = addAresta(grafo, id, vizinho);
+		}while(rc == 1);
+	}
 	return initGrafo(1);
+error:
+	return NULL;
 }
